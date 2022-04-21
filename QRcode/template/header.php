0f0f692e9de901d89f9e44f6201fd45d5dcf00ea
Archiverofgongkaiwen <46291059+Archiverofgongkaiwen@users.noreply.github.com>
2022-04-21 09:52:33 -0400

    Add files via upload
    QRCode

---
+++ QRcode/template/header.php
@@ -0,0 +1,8 @@
+<div class="bg-primary position-relative">
+  <div class="overlay-gradient"></div>
+  <div class="container position-relative">
+  	<div class="row py-5">
+    </div>
+    </div>
+  </div>
+</div>
